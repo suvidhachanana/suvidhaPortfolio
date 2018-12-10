c47b684e9884fdddd7c881543dc9ac1361d9a781
suvidhachanana <schanana@northislandcollege.ca>
2018-12-10 13:22:51 +0530

    enabled debugging in config.php

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,16 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+ // Enable WP_DEBUG mode
+ define('WP_DEBUG', true);
+
+ // Enable Debug logging to the /wp-content/debug.log file
+ define('WP_DEBUG_LOG', true);
+
+ // Enable display of errors and warnings
+ define('WP_DEBUG_DISPLAY', true);
+ @ini_set('display_errors',0);
+
 
 /* That's all, stop editing! Happy blogging. */
 
